17d1a4d213b7a8f9a6319a3c7456c4dd133a50b8
acaValkyrie <acavalkyrie@gmail.com>
2024-07-31 10:39:42 +0900

    neopixel動作確認まで実装

--- src/main.cpp
+++ src/main.cpp
@@ -3,77 +3,17 @@
 
 const int pin = C4;
 
-#define T0H 0.25
-#define T1H 0.6
-#define T0L 1.0
-#define T1L 0.65
+static uint8_t gpioForPin(pin_size_t pin);
+static GPIO_TypeDef* gpioRegister(uint8_t gpio);
+static uint8_t gpioPin(uint8_t gpio, pin_size_t pin);
+// void code_0(GPIO_TypeDef* port, uint8_t p);
+// void code_1(GPIO_TypeDef* port, uint8_t p);
+void code_res(int pin);
+void intToBitArray(int value, int* bitArray, int bitArraySize);
+void makeCommand(int* command, int r, int g, int b);
+void sendCommand(int pin, int* command, unsigned int length);
+void neoPixelWrite(int pin, int r, int g, int b);
 
-#define NOP asm("nop")
-
-void code_0(int pin){
-  digitalWrite(pin, HIGH);
-  Delay_Us(T0H);
-  NOP;
-  digitalWrite(pin, LOW);
-  Delay_Us(T0L);
-}
-
-void code_1(int pin){
-  digitalWrite(pin, HIGH);
-  Delay_Us(T1H);
-  digitalWrite(pin, LOW);
-  Delay_Us(T1L);
-}
-
-void code_res(int pin){
-  digitalWrite(pin, LOW);
-  Delay_Us(100);
-}
-
-void intToBitArray(uint value, char* bitArray, int bitArraySize) {
-  for (int i = 0; i < bitArraySize; i++) {
-    bitArray[bitArraySize - 1 - i] = (value & (1 << i)) ? '1' : '0';
-  }
-  bitArray[bitArraySize] = '\0'; // Null-terminate the string
-}
-
-void makeCommand(char* command, int r, int g, int b){
-  char red_str[8];
-  char green_str[8];
-  char blue_str[8];
-  // uint to bit array
-  intToBitArray(r, red_str, 8);
-  intToBitArray(g, green_str, 8);
-  intToBitArray(b, blue_str, 8);
-  for(int i = 0; i < 8; i++){
-    command[i] = red_str[i];
-    command[i+8] = green_str[i];
-    command[i+16] = blue_str[i];
-  }
-}
-
-void sendCommand(int pin, char* command, unsigned int length){
-  for(unsigned int i = 0; i < length; i++){
-    if(command[i] == 0){
-      code_0(pin);
-    }else{
-      code_1(pin);
-    }
-  }
-  code_res(pin);
-}
-
-void neoPixelWrite(int pin, int r, int g, int b){
-  char command[24];
-  makeCommand(&command[0], r, g, b);
-  sendCommand(pin, &command[0], sizeof(command)/sizeof(command[0]));
-  
-}
-
-void setup(){
-  SETUP_SYSTICK_HCLK;
-  pinMode(pin, OUTPUT);
-}
 
 static uint8_t gpioForPin(pin_size_t pin){
   if(pin < 2){
@@ -108,26 +48,136 @@ static uint8_t gpioPin(uint8_t gpio, pin_size_t pin){
   }
 }
 
-void loop(){
-  // for(int i = 0; i < 255; i++){
-  //   neoPixelWrite(pin, 0, 0, i);
-  //   delay(10);
-  // }
-  // neopixel_write(GPIOC, GPIO_Pin_4, colorData, LED_NUM);
+void code_res(int pin){
+  digitalWrite(pin, LOW);
+  Delay_Us(100);
+}
+
+void intToBitArray(int value, int* bitArray, int bitArraySize) {
+  for (int i = 0; i < bitArraySize; i++) {
+    bitArray[bitArraySize - 1 - i] = (value & (1 << i)) ? 1 : 0;
+  }
+}
+
+void makeCommand(int* command, int r, int g, int b){
+  int red_str[8];
+  int green_str[8];
+  int blue_str[8];
+  // uint to bit array
+  intToBitArray(r, red_str, 8);
+  intToBitArray(g, green_str, 8);
+  intToBitArray(b, blue_str, 8);
+  for(int i = 0; i < 8; i++){
+    command[i] = red_str[i];
+    command[i+8] = green_str[i];
+    command[i+16] = blue_str[i];
+  }
+}
+
+#define T0H 0.25
+#define T1H 0.6
+#define T0L 1.0
+#define T1L 0.65
+
+#define code_0(port, p) \
+  port->BSHR = 1 << p; \
+  for(int i = 0; i < 8; i++) _NOP(); \
+  for(int i = 0; i < 7; i++) _NOP(); \
+  port->BCR = 1 << p; \
+  for(int i = 0; i < 8; i++) _NOP(); \
+  for(int i = 0; i < 8; i++) _NOP(); \
+  for(int i = 0; i < 8; i++) _NOP(); \
+  for(int i = 0; i < 8; i++) _NOP(); \
+  for(int i = 0; i < 8; i++) _NOP(); \
+  for(int i = 0; i < 8; i++) _NOP(); \
+  for(int i = 0; i < 8; i++) _NOP(); \
+  for(int i = 0; i < 6; i++) _NOP();
+
+#define code_1(port, p) \
+  port->BSHR = 1 << p; \
+  for(int i = 0; i < 8; i++) _NOP(); \
+  for(int i = 0; i < 8; i++) _NOP(); \
+  for(int i = 0; i < 8; i++) _NOP(); \
+  for(int i = 0; i < 8; i++) _NOP(); \
+  for(int i = 0; i < 8; i++) _NOP(); \
+  for(int i = 0; i < 8; i++) _NOP(); \
+  for(int i = 0; i < 8; i++) _NOP(); \
+  for(int i = 0; i < 8; i++) _NOP(); \
+  port->BCR = 1 << p; \
+  for(int i = 0; i < 1; i++) _NOP(); 
+
+#define code_IO(port, p, value) \
+  if(value == 0){ code_0(port, p); }else if(value == 1){ code_1(port, p);}
+
+void sendCommand(int pin, int* command, unsigned int length){
   uint8_t gpio = gpioForPin(pin);
   GPIO_TypeDef* port = gpioRegister(gpio);
   uint8_t p = gpioPin(gpio, pin);
+  int v0 = command[0];
+  int v1 = command[1];
+  int v2 = command[2];
+  int v3 = command[3];
+  int v4 = command[4];
+  int v5 = command[5];
+  int v6 = command[6];
+  int v7 = command[7];
+  int v8 = command[8];
+  int v9 = command[9];
+  int v10 = command[10];
+  int v11 = command[11];
+  int v12 = command[12];
+  int v13 = command[13];
+  int v14 = command[14];
+  int v15 = command[15];
+  int v16 = command[16];
+  int v17 = command[17];
+  int v18 = command[18];
+  int v19 = command[19];
+  int v20 = command[20];
+  int v21 = command[21];
+  int v22 = command[22];
+  int v23 = command[23];
+  code_IO(port, p, v0);
+  code_IO(port, p, v1);
+  code_IO(port, p, v2);
+  code_IO(port, p, v3);
+  code_IO(port, p, v4);
+  code_IO(port, p, v5);
+  code_IO(port, p, v6);
+  code_IO(port, p, v7);
+  code_IO(port, p, v8);
+  code_IO(port, p, v9);
+  code_IO(port, p, v10);
+  code_IO(port, p, v11);
+  code_IO(port, p, v12);
+  code_IO(port, p, v13);
+  code_IO(port, p, v14);
+  code_IO(port, p, v15);
+  code_IO(port, p, v16);
+  code_IO(port, p, v17);
+  code_IO(port, p, v18);
+  code_IO(port, p, v19);
+  code_IO(port, p, v20);
+  code_IO(port, p, v21);
+  code_IO(port, p, v22);
+  code_IO(port, p, v23);
 
-  port->BSHR = 1 << p;
-  _NOP();
-  port->BCR = 1 << p;
-  _NOP();
-  port->BSHR = 1 << p;
-  _NOP();
-  port->BCR = 1 << p;
-  _NOP();
-  port->BSHR = 1 << p;
-  _NOP();
-  port->BCR = 1 << p;
-  _NOP();
+  code_IO(port, p, 0);
+}
+
+void neoPixelWrite(int pin, int r, int g, int b){
+  int command[24];
+  makeCommand(command, r, g, b);
+  sendCommand(pin, command, 24);
+  
+}
+
+void setup(){
+  SETUP_SYSTICK_HCLK;
+  pinMode(pin, OUTPUT);
+}
+
+void loop(){
+  neoPixelWrite(pin, 10,10,10);
+  delay(100);
 }
--- test/test.cpp
+++ test/test.cpp
@@ -1,15 +1,15 @@
-void intToBitArray(unsigned int value, char* bitArray, int bitArraySize) {
+void intToBitArray(int value, int* bitArray, int bitArraySize) {
   for (int i = 0; i < bitArraySize; i++) {
     bitArray[bitArraySize - 1 - i] = (value & (1 << i)) ? '1' : '0';
   }
   bitArray[bitArraySize] = '\0'; // Null-terminate the string
 }
 
-void makeCommand(char* command, int r, int g, int b){
-  char red_str[8];
-  char green_str[8];
-  char blue_str[8];
-  // unsigned int to bit array
+void makeCommand(int* command, int r, int g, int b){
+  int red_str[8];
+  int green_str[8];
+  int blue_str[8];
+  // uint to bit array
   intToBitArray(r, red_str, 8);
   intToBitArray(g, green_str, 8);
   intToBitArray(b, blue_str, 8);
@@ -19,10 +19,11 @@ void makeCommand(char* command, int r, int g, int b){
     command[i+16] = blue_str[i];
   }
 }
+
 #include <stdio.h>
 int main(){
-  char command[24];
-  makeCommand(&command[0], 0,0,255);
+  int command[24];
+  makeCommand(command, 128,128,255);
   for(int i = 0; i < 24; i++){
     printf("%c", command[i]);
   }
